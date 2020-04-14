a572b77d06ee2d188fa1a9a1838fa66d03fbd53c
Nick Mudge <nick@perfectabstractions.com>
2020-04-14 17:29:35 -0400

    Changed deps.js

--- deps.js
+++ deps.js
@@ -1,9 +1,7 @@
-export * as mozilla from "https://raw.githubusercontent.com/mudgen/deno-source-map/master/source-map.js";
+export * as mozilla from "https://raw.githubusercontent.com/denolib/deno-source-map/master/source-map.js";
 
 export * as path from "https://deno.land/std/node/path.ts";
 
 export * as chalk from "https://deno.land/std/fmt/colors.ts";
 
 export * as fs from "https://deno.land/std/node/fs.ts";
-
-
